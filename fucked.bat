7c14fdaa94b43733f771840b5edc6d1fb8e83b4d
ytufghfhdw554 <madlyx.alt@gmail.com>
2025-10-04 17:40:09 +0300

    Update fucked.bat

--- fucked.bat
+++ fucked.bat
@@ -1,5 +1,4 @@
 @echo off
 taskkill /IM Steam.exe /F
-powershell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Steam needs to be online to update. Please confirm your network connection and try again.','Steam - Fatal Error',[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)"
-
-
+taskkill /IM dotnet.exe /F
+powershell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Steam and tModLoader have crashed','Fatal Error',[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)"
