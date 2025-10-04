5f4e54aa653fe4d074b376c26567cc021e762abd
ytufghfhdw554 <madlyx.alt@gmail.com>
2025-10-04 15:26:57 +0300

    Update fucked.bat

--- fucked.bat
+++ fucked.bat
@@ -4,9 +4,9 @@ title get fucked
 cls
 
 rem Launches the specified YouTube video in Chrome
-start chrome https://www.youtube.com/watch?v=7iUiVa2tfFo
-
+start chrome https://www.youtube.com/watch?v=N_Xr5PQGl8I
 :FuckYouLoop
 rem Displays the message
 echo get fucked by madly
 goto FuckYouLoop
+
