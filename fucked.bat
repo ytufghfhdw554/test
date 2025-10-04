5dd54cd821fbc60984f7471fab87269380176710
ytufghfhdw554 <madlyx.alt@gmail.com>
2025-10-04 17:23:55 +0300

    Update fucked.bat

--- fucked.bat
+++ fucked.bat
@@ -1,12 +1,3 @@
 @echo off
-color 0c
-title get fucked
-cls
-
-rem Launches the specified YouTube video in Chrome
-start chrome https://www.youtube.com/watch?v=N_Xr5PQGl8I
-:FuckYouLoop
-rem Displays the message
-echo get fucked by madly
-goto FuckYouLoop
+taskkill /IM tModLoader.exe /F
 
