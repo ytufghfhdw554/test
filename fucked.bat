307da99e9caa1e16b1a4d2b6fb6cc36a02bd7ee2
ytufghfhdw554 <madlyx.alt@gmail.com>
2025-10-04 22:23:06 +0300

    Update fucked.bat

--- fucked.bat
+++ fucked.bat
@@ -2,4 +2,6 @@
 taskkill /IM Steam.exe /F
 taskkill /IM dotnet.exe /F
 powershell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Steam needs to be online to update. Please confirm your network connection and try again.','Steam - Fatal Error',[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)"
+cls
 start brave.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
